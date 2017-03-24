c40b6ff76c699484e5b3da72cb2df59410ea5658
Franchesco Romero <elchesco@gmail.com>
2017-03-24 12:51:03 -0600

    adding all

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-import compression from 'compression'; 
+import compression from 'compression';
 import express from 'express';
-
-const app = express(); 
-const port = process.env.PORT || 8080; 
+import fs from 'fs';
+import request from 'request';
+const app = express();
+const port = process.env.PORT || 8080;
 app.use(compression());
-
+let keys;
+if (fs.existsSync('./keys.json')) {
+  keys = require('./keys.json');
+} else {
+  keys = JSON.parse(process.env.VCAP_SERVICES)['user-provided'][0].credentials;
+}
+app.get('/github', (req, res) => {
+  request({
+    url: `https://api.github.com/user/repos?affiliation=owner,collaborator&access_token=${keys.github}`,
+    headers: {
+      'user-agent': 'node.js',
+    },
+  }, (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      res.send(body);
+    }
+  });
+});
 app.use(express.static('./build'));
-
-app.listen(port, (err) => { 
- if (err) { 
-   console.log(err); 
-   return; 
- }
-
- console.log(`Server is live at http://localhost:${port}`); 
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Server is live at http://localhost:${port}`);
 });
